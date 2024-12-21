23e39de99e5dd7c842a5f61b636df68491a89a49
NuraidaKashmin <nuraidakkashmin@gmail.com>
2024-12-22 00:42:49 +0600

    added navbar component

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,11 +1,20 @@
 import {
     createBrowserRouter,
   } from "react-router-dom";
+import MainLayout from "../layout/MainLayout";
+import Home from "../pages/Home";
 
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <div>Hello world!</div>,
+      element: <MainLayout></MainLayout>,
+      errorElement: <h2>Route not found</h2>,
+      children: [
+        {
+            path: '/',
+            element: <Home></Home>
+        }
+      ]
     },
   ]);
 
