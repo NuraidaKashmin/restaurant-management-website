e54018d38b7a4c3125ca88dbb4f3ff50694c2805
NuraidaKashmin <nuraidakkashmin@gmail.com>
2024-12-22 19:30:16 +0600

    added private route

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,6 +1,6 @@
 import {
-    createBrowserRouter,
-  } from "react-router-dom";
+  createBrowserRouter,
+} from "react-router-dom";
 import MainLayout from "../layout/MainLayout";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
@@ -12,56 +12,61 @@ import MyFood from "../pages/MyFood";
 import AddFood from "../pages/AddFood";
 import MyOrders from "../pages/MyOrders";
 import SingleFoodPage from "../pages/SingleFoodPage";
+import PrivateRoute from "./PrivateRoute";
 
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <MainLayout></MainLayout>,
-      errorElement: <h2>Route not found</h2>,
-      children: [
-        {
-            path: '/',
-            element: <Home></Home>
-        },
-        {
-            path: 'all-foods',
-            element: <AllFoods></AllFoods>
-        },
-        {
-            path: 'gallery',
-            element:<Gallery></Gallery>
-        },
-        {
-            path: 'food-purchased',
-            element: <FoodPurchased></FoodPurchased>
-        },
-        {
-          path: 'single-food-page',
-          element: <SingleFoodPage></SingleFoodPage>
-        },
-        {
-          path: 'my-food',
-          element: <MyFood></MyFood>
-        },
-        {
-          path: 'add-food',
-          element: <AddFood></AddFood>
-        },
-        {
-          path: 'my-orders',
-          element:<MyOrders></MyOrders>
-        },
-        {
-            path: 'login',
-            element: <Login></Login>
-        },
-        {
-            path: 'register',
-            element: <Register></Register>
-        }
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout></MainLayout>,
+    errorElement: <h2>Route not found</h2>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      },
+      {
+        path: 'all-foods',
+        element: <AllFoods></AllFoods>
+      },
+      {
+        path: 'gallery',
+        element: <Gallery></Gallery>
+      },
+      {
+        path: 'food-purchased',
+        element: (
+          <PrivateRoute>
+            <FoodPurchased></FoodPurchased>
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: 'single-food-page',
+        element: <SingleFoodPage></SingleFoodPage>
+      },
+      {
+        path: 'my-food',
+        element: <MyFood></MyFood>
+      },
+      {
+        path: 'add-food',
+        element: <AddFood></AddFood>
+      },
+      {
+        path: 'my-orders',
+        element: <MyOrders></MyOrders>
+      },
+      {
+        path: 'login',
+        element: <Login></Login>
+      },
+      {
+        path: 'register',
+        element: <Register></Register>
+      }
 
-      ]
-    },
-  ]);
+    ]
+  },
+]);
 
-  export default router;
+export default router;
